6fe48b2c8a0813f9a5bea7b59bffab5a2d63f6b6
Martijn van de Rijdt <martijn@enketo.org>
2018-01-02 15:48:31 -0700

    changed: refactored interpolation

--- tools/interpolate.js
+++ tools/interpolate.js
@@ -1,14 +1,12 @@
 'use strict';
-let navigation = require( '../src/js/navigation' );
+const navigation = require( '../src/js/navigation' );
 
-module.exports = ( text ) => {
-    if ( typeof text === 'string' ) {
-        for ( let key in navigation.sites ) {
-            text = text.replace( new RegExp( '#{\\s?' + key + '\\s?}', 'gi' ), navigation.sites[ key ] );
-        }
-        if ( typeof text !== 'undefined' && text.indexOf( '#' ) === -1 && text.indexOf( '?' ) === -1 && ( text.length === 0 || text.lastIndexOf( '/' ) !== text.length - 1 ) ) {
-            text += '/';
-        }
+module.exports = (text = '') => {
+    for ( let key in navigation.sites ) {
+        text = text.replace( new RegExp( '#{\\s?' + key + '\\s?}', 'gi' ), navigation.sites[ key ] );
+    }
+    if ( !text.includes( '#' ) && !text.includes( '?' ) && !(/\/$/.test(text)) ) {
+        text += '/';
     }
     return text;
 };
