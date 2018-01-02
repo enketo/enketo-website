ac6caf3684785f969032189e53c6e5b792f9861c
Martijn van de Rijdt <martijn@enketo.org>
2018-01-02 16:46:23 -0700

    changed: refactored markdown options

--- tools/options.js
+++ tools/options.js
@@ -1,13 +1,34 @@
 'use strict';
 
-let navigation = require( '../src/js/navigation' );
-let interpolate = require( './interpolate' );
-let hljs = require( 'highlight.js' );
-let marked = require( 'marked' );
-let pkg = require( '../package' );
-let renderer = new marked.Renderer();
-let lookupNavItem;
-let replaceIcons;
+const navigation = require( '../src/js/navigation' );
+const interpolate = require( './interpolate' );
+const hljs = require( 'highlight.js' );
+const marked = require( 'marked' );
+const pkg = require( '../package' );
+const renderer = new marked.Renderer();
+
+// Replace icon references
+const replaceIcons = ( text ) => text.replace( ':check:', '<span class="icon icon-check"></span>' ).replace( ':question:', '<span class="icon icon-question"></span>' );
+
+// Replace navigation.json references
+const lookupNavItem = ( keyStr ) => {
+    return keyStr.replace( /(primary|sites)(\.|\[).+/, ( match ) => {
+        let item = navigation;
+        const props = match.split( /\s*\.|\[\s*&quot;|&quot;\s*\]|\[\s*&#39;|&#39;\s*\]\s*/ );
+
+        props.forEach( ( prop ) => {
+            if ( prop ) {
+                item = item && typeof item[ prop ] !== 'undefined' ? item[ prop ] : null;
+            }
+        } );
+
+        if ( typeof item === 'undefined' || item === null ) {
+            throw new Error( 'Trouble finding link: ' + match );
+        }
+
+        return interpolate( item );
+    } );
+};
 
 // add highlightClass on wrapping <pre> (this is a missing feature is setOptions is used: https://github.com/chjj/marked/pull/418)
 renderer.code = ( code, language ) => `<pre><code class="hljs ${language}">${hljs.highlightAuto(code, ['xml']).value}</code></pre>`;
@@ -19,49 +40,22 @@ renderer.heading = ( text, level ) => '<h' + level + '>' + text + '</h' + level 
 renderer.link = ( href, title, text ) => `<a href="${lookupNavItem( href )}"${title ? ` title="${title}"` : ''}>${text}</a>`;
 
 // support :check: and :question: in table-cells, removes flags.align support as we don't need it.
-renderer.tablecell = (content, flags) => {
+renderer.tablecell = ( content, flags ) => {
     let type = flags.header ? 'th' : 'td';
     return `<${type}>${replaceIcons(content)}</${type}>\n`;
 };
 
-replaceIcons = (text) => text.replace(':check:', '<span class="icon icon-check"></span>').replace(':question:', '<span class="icon icon-question"></span>');
-
-// Replace navigation.json references
-lookupNavItem = ( keyStr ) => {
-    return keyStr.replace( /(primary|sites)(\.|\[).+/, ( match ) => {
-        let item = navigation;
-        let props = match.split(   /\s*\.|\[\s*&quot;|&quot;\s*\]|\[\s*&#39;|&#39;\s*\]\s*/ );
-        
-        props.forEach( ( prop ) => {
-            if ( prop ) {
-                item = item && typeof item[ prop ] !== 'undefined' ? item[ prop ] : null;
-            } 
-        } );
-
-        if (typeof item === 'undefined' || item === null ) {
-            throw new Error('Trouble finding link: ' + match);
-        }
-
-        return interpolate( item );
-    } );
-}
-
 module.exports = {
     filters: {
-        md: function( text, options ) {
-            return marked( text, {
-                renderer: renderer
-            } );
-        },
-        'samplify': function( text, options ) {
+        md: ( text, options ) => marked( text, { renderer: renderer } ),
+        samplify: ( text, options ) => {
             let result = '';
-            let arr = JSON.parse( text );
-            let l = arr.length;
-            let srcLink;
+            const arr = JSON.parse( text );
+            const l = arr.length;
 
             // prepend beforeLast and last, append first and second
             [ arr[ l - 2 ], arr[ l - 1 ] ].concat( arr ).concat( [ arr[ 0 ], arr[ 1 ] ] ).forEach( function( item, index ) {
-                srcLink = item.src ? `<p><a href="${item.src}">source</a></p>` : '';
+                const srcLink = item.src ? `<p><a href="${item.src}">source</a></p>` : '';
                 result +=
                     `<li class="samples__list__item">
                         <section class="samples__list__item__main">
